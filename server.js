27b68ad69a0928929f4ee25d2b2c95e6203c52e7
Daniel Kowalczyk <kostex2@gmail.com>
2026-04-15 09:53:04 +0100

    Add MongoDB session store

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const router = express.Router();
 const adsRoutes = require('./routes/ads.routes');
 const authRoutes = require('./routes/auth.routes');
 const session = require('express-session');
+const { MongoStore } = require('connect-mongo');
 
 require('./models/Ad.model');
 require('./models/User.model');
 
 dotenv.config();
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server is runing on port: ${PORT}`)
-});
 
 app.use(express.json());
-app.use(session({ secret: 'supersecret', resave: false, saveUninitialized: false }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, store: MongoStore.create({ mongoUrl: process.env.DB_URL }) }));
 app.use('/api/ads', adsRoutes);
 app.use('/auth', authRoutes);
 
@@ -29,5 +27,11 @@ app.get('/api/test', (req, res) => {
 
 
 mongoose.connect(process.env.DB_URL)
-    .then(() => console.log('Connected to the database'))
+    .then(() => {
+        console.log('Connected to the database');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    })
     .catch(err => console.log('Error: ' + err));
